49cafdb93f9fb5b24857f7bef83d4dc79c159336
Rohan Elukurthy <roh3@pdx.edu>
2015-10-23 23:53:41 -0700

    adding the circle hilight on click and making a var for lat,lng

---
+++ mapEvents.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Rohan on 10/23/2015.
+ */
+
+function myApp(){
+    var noOfMiles = 3;
+    var noOfMetres = noOfMiles * 1609.34;
+    console.log(window.map);
+    window.map.addListener('click', function(p) {
+        if (window.lastCircle != null){
+            window.lastCircle.setMap(null);
+        }
+        var myPos = {lat: p.latLng.lat(), lng: p.latLng.lng()};
+        console.log(p);
+        console.log(p.latLng.lat(), p.latLng.lng());
+
+        // Add the circle for this city to the map.
+        window.lastCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: myPos,
+            radius: noOfMetres
+        });
+    });
+
+}
