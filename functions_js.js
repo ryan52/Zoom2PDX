133cb5a147c226947a89fea6e2c934d1fbd7dd64
kittie <karis4@pdx.edu>
2015-10-23 23:35:09 -0700

    put algorithm into functions_js.js

--- functions_js.js
+++ functions_js.js
@@ -30,6 +30,46 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 		'Error: Your browser doesn\'t support geolocation.');
 }
 
+function calculate_score(school) {
+
+    var total = 0;
+
+    var res = school.SchoolInformation.Dropouts.DropoutStudentsPctCurrYear - school.SchoolInformation.Dropouts.DropoutStudentsPrevYearPct;
+    if (res < 0) {
+        ++total;
+    }
+
+    res = school.SchoolInformation.Attendance.AttendancePctCurrYear - school.SchoolInformation.Attendance.AttendancePctPrevYear;
+    if (res > 0) {
+        ++total;
+    }
+
+    res = school.SchoolInformation.StudentPopulation.NumberOfStudents / school.SchoolInformation.Staffing.Teachers;
+    if (res < 15) {
+        total += 2;
+    } else if (res < 25) {
+        ++total;
+    }
+
+    if (school.SchoolInformation.Staffing.MastersDegreeOrHigherPct > 50) {
+        total += 3;
+    } else if (school.SchoolInformation.Staffing.MastersDegreeOrHigherPct > 35) {
+        total += 2;
+    } else if (school.SchoolInformation.Staffing.MastersDegreeOrHigherPct > 20) {
+        ++total;
+    }
+
+    if (school.SchoolInformation.Staffing.AvgYearsExperience > 15) {
+        total += 3;
+    } else if (school.SchoolInformation.Staffing.AvgYearsExperience > 10) {
+        total += 2;
+    } else if (school.SchoolInformation.Staffing.AvgYearsExperience > 5) {
+        ++total;
+    }
+
+    return total;
+}
+
 function get_school_data(school_id){
 	var schoolid = "/"+school_id || "";
 	return $.ajax({
